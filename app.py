78521cc2af89c2d492b40fe028a762fd2a9a50be
Punit Arani <punitsai36@gmail.com>
2023-06-10 11:02:24 -0700

    Add hand tracking

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 """Streamlit App"""
 
 import cv2
+import mediapipe as mp
 
 # Initialize webcam video capture
 cap = cv2.VideoCapture(0)
@@ -10,11 +11,29 @@ cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
 cap.set(cv2.CAP_PROP_FPS, 30)
 
+# Initialize mediapipe hand detection
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.7)
+mp_draw = mp.solutions.drawing_utils
+
 if __name__ == "__main__":
     while True:
-        # Read webcam footage frame by frame and display it
+        # Get the latest frame from the webcam
         ret, frame = cap.read()
-        cv2.imshow('Webcam Footage', frame)
+
+        # Convert the frame to RGB (MediaPipe uses RGB images)
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # Process the frame
+        results = hands.process(frame_rgb)
+
+        # Draw the hand landmarks on the frame
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+        # Display the frame
+        cv2.imshow("Hand Tracking", frame)
 
         # Exit on pressing 'q' key
         if cv2.waitKey(1) & 0xFF == ord('q'):
